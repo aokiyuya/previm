3ccc11b664629ab71fbe034539430d4797df814e
aokiyuya <yuya.aoki@fujielab.org>
2017-09-27 15:21:46 +0900

    comment out

--- preview/js/previm.js
+++ preview/js/previm.js
@@ -93,7 +93,7 @@
       Array.prototype.forEach.call(_doc.querySelectorAll('pre code'), hljs.highlightBlock);
       autoScroll('body', beforePageYOffset);
       style_header();
-      MathJax.Hub.Typeset(document.getElementById("preview"));
+      // MathJax.Hub.Typeset(document.getElementById("preview"));
     }
   }
 
